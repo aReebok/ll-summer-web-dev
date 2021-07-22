4f5b79466ae054bb1db800602a75c0d6d69a0831
Areeba Khan <khan6@stolaf.edu>
2021-07-22 10:30:55 -0500

    Removes/adds comments and updates MAX_TABLE and for loop to scrape 5 or less expressions

--- regex-search/bs4_test.py
+++ regex-search/bs4_test.py
@@ -1,45 +1,42 @@
 from bs4 import BeautifulSoup
 import requests
 
-#
-# data storing class of Table that stores attributes:
-# expression name, expression, description and author
+'''
+    Creates a data storing class of Table that stores attributes:
+    expression name, expression, description and author
+'''
 class Table: 
     def __init__(self, exp_name, exp, description, author):
         self.exp_name = exp_name
         self.exp = exp
-        # if len(description) > 200:
-        #     description = description[0:125]
         description =  description.split('\n')[0]
         self.description = description
         self.author = author
     
+    '''
+    writes content into a csv file for given table
+    '''
     def write_into_file(self):
         print("Writing attributes into csv file...")        
     
-    # def print_desc(self):
-    #     temp_desc = self.description.split('\n')[0]
-    #     print(temp_desc) 
-
+    '''
+    displays the table class objects
+    '''
     def print_table(self):
         print("Title: " + self.exp_name)
         print("Expression: " + self.exp)
-
         print("Description: " + self.description)
-
         print("Author: " + self.author)
         print("------xxx------")
 
-#
-# GLOBAL VARS
-MAX_TABLE = 5
+''' GLOBAL VARS'''
+MAX_TABLE = 1 #100      # the number of data that will be added to csv
 
 def create_table(table):
     try:
         all_as = table.find_all('a')        # creates an array of all a tags
-        
         exp_name = all_as[2].text.strip()   # index [2] holds expression name
-        exp_author = all_as[3].text.strip() # index [3] holds expression author
+        exp_author = all_as[len(all_as) - 1].text.strip() # index [last item] holds expression author
 
         expression = table.find('div', class_="expressionDiv").text.strip()
         description = table.find('div', class_="overflowFixDiv").text.strip()
@@ -50,28 +47,21 @@ def create_table(table):
     table = Table(exp_name, expression, description, exp_author)
     return table
 
-#
-# web scraping
-webpage = "https://regexlib.com/Search.aspx?k=password&c=-1&m=-1&ps=20&AspxAutoDetectCookieSupport=1"
+'''web scraping code
+given a webpage, up to 5 tables are copied and placed into a csv file'''
+webpage = "https://regexlib.com/Search.aspx?k=email&c=-1&m=-1&ps=20"
 source = requests.get(webpage).text
 
 soup = BeautifulSoup(source, 'lxml')
 table = soup.find_all('table')
 
 list_tables = []
-list_tables.append(create_table(table[4]))
 
-# count = 0
-# while(count != MAX_TABLE ):
-#     list_tables.append(table[count])    # list_tables[count].print_table()
-#     count += 1
+if MAX_TABLE > len(table):
+    MAX_TABLE = len(table)
 
-# print("Number of items in the list = ", len(table))
-# print("Number of items in created list = ", len(list_tables))
-
+for i in range(MAX_TABLE):
+    list_tables.append(create_table(table[i]))
 
 for tab in list_tables:
     tab.print_table()
-    # tab.print_desc()
-
-# print(list_tables)
