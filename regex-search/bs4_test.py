09a9a8006fbdc3ac34d768ae639b01cb047a6dbc
Areeba Khan <khan6@stolaf.edu>
2021-07-21 19:31:08 -0500

    Adds loops and 5 tables are added

--- regex-search/bs4_test.py
+++ regex-search/bs4_test.py
@@ -14,34 +14,60 @@ class Table:
     def write_into_file(self):
         print("Writing attributes into csv file...")
 
+    def check_len(self):
+        if len(self.description) > 20:
+            self.description = self.description[0:20]
+
     def print_table(self):
         print("Title: " + self.exp_name)
         print("Expression: " + self.exp)
         print("Description: " + self.description)
         print("Author: " + self.author)
         print("------xxx------")
+
+#
+# GLOBAL VARS
+MAX_TABLE = 5
+
+def create_table(table):
+    try:
+        all_as = table.find_all('a')        # creates an array of all a tags
+        
+        exp_name = all_as[2].text.strip()   # index [2] holds expression name
+        exp_author = all_as[3].text.strip() # index [3] holds expression author
+
+        expression = table.find('div', class_="expressionDiv").text.strip()
+        description = table.find('div', class_="overflowFixDiv").text.strip()
+
+
+
+    except Exception as e:
+        exp_name, exp_author, expression, description = '' 
+
+    table = Table(exp_name, expression, description, exp_author)
+    return table
+
 #
 # web scraping
 webpage = "https://regexlib.com/Search.aspx?k=password&c=-1&m=-1&ps=20&AspxAutoDetectCookieSupport=1"
 source = requests.get(webpage).text
 
 soup = BeautifulSoup(source, 'lxml')
-table = soup.find('table')
+table = soup.find_all('table')
 
-count = 0
-for a in table.find_all('a'):
-    count += 1
-    if(count == 3):
-        exp_name = a.text.strip()
-        # 
-    if(count == 4):
-        exp_author = a.text.strip()
-        # 
-expression = table.find('div', class_="expressionDiv").text.strip()
-# print(expression.text)
+list_tables = []
+list_tables.append(create_table(table[4]))
 
-description = table.find('div', class_="overflowFixDiv").text.strip()
-# print(description.text)
+# count = 0
+# while(count != MAX_TABLE ):
+#     list_tables.append(table[count])    # list_tables[count].print_table()
+#     count += 1
 
-table = Table(exp_name, expression, description, exp_author)
-table.print_table()
+# print("Number of items in the list = ", len(table))
+# print("Number of items in created list = ", len(list_tables))
+
+
+for tab in list_tables:
+    tab.print_table()
+
+# print(list_tables)
