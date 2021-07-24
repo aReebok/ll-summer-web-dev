b82ad315d4757723b90985a14f32833c01568d07
Areeba Khan <khan6@stolaf.edu>
2021-07-24 12:44:42 -0500

    Shortens get row func

--- regex-search/scrape_regexlib.py
+++ regex-search/scrape_regexlib.py
@@ -1,8 +1,16 @@
+
+'''
+This .py takes the 'user_input.txt' file, appends it to the RegExLib.com search link,
+and scrapes up to MAX_TABLE different expressions and it's details. Finally, it 
+writes these out into a file called 'pg_content.txt' for other files to use.
+'''
+
 from os import link
 from bs4 import BeautifulSoup
 import requests
 
 import csv
+
 
 ''' GLOBAL VARS'''
 MAX_TABLE = 5 #100      # the number of data that will be added to csv
@@ -30,19 +38,12 @@ class Table:
 
         self.link = link
         self.author = author.replace(',', '')
-    
+
     '''
     writes content into a csv file for given table
     '''
     def get_row(self):
-        row = []
-        row.append(self.exp_name)
-        row.append(self.exp)
-        row.append(self.description)
-        row.append(self.link)
-        row.append(self.author)
-
-        return row
+        return [self.exp_name, self.exp, self.description, self.link, self.author]
 
     '''
     displays the table class objects
@@ -74,10 +75,13 @@ def create_table(table):
 
 '''web scraping code
 given a webpage, up to 5 tables are copied and placed into a csv file'''
-webpage = "https://regexlib.com/Search.aspx?k=password&c=-1&m=-1&ps=20"
+webpage = 'https://regexlib.com/Search.aspx?k= &c=-1&m=-1&ps=20'.split(' ')
+f = open('user_input.txt', 'r')     # creates webpage link from an input file
+webpage = webpage[0] + f.readline() + webpage[1] 
+
 source = requests.get(webpage).text
 
-soup = BeautifulSoup(source, 'lxml')
+soup = BeautifulSoup(source, 'lxml')    # create BS obj of article 
 table = soup.find_all('table')
 
 list_tables = []
@@ -91,7 +95,6 @@ writer = csv.writer(f)
 count = 0
 for i in range(MAX_TABLE):
     list_tables.append(create_table(table[i]))
-    # print("HERE LINE 94: " + list_tables[count].exp)
     if list_tables[count].exp != '': 
         writer.writerow(list_tables[count].get_row())
     count += 1
